28231a9b3441ac29f3b8c187d4add2134108b452
milkiascha7 <mikehailu504@gmail.com>
2022-12-28 11:07:38 +0300

    "add navbar and sidebar, themes with redux"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useMemo } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+import { createTheme } from '@mui/material/styles';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Layout from 'scenes/layout';
+import Dashboard from 'scenes/dashboard';
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
