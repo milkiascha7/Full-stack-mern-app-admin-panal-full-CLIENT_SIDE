92511dbff65ca17ad3af97cfe518f52739dc7ed2
milkiascha7 <mikehailu504@gmail.com>
2022-12-29 12:47:18 +0300

    "header component, getproduct api route in rtk"

--- src/state/api.js
+++ src/state/api.js
@@ -3,13 +3,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: 'adminApi',
-  tagTypes: ['User'],
+  tagTypes: ['User', 'Products'],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTages: ['User'],
     }),
+    getProducts: build.query({
+      query: () => 'client/products',
+      providesTages: ['Products'],
+    }),
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
