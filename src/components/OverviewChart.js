f0b6b6514324fdb1c0be83775b2544333bb976c8
milkiascha7 <mikehailu504@gmail.com>
2022-12-30 23:59:13 +0300

    "overview page start"

---
+++ src/components/OverviewChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import { useTheme } from '@mui/material';
+import { useGetSalesQuery } from 'state/api';
+
+const OverviewChart = () => {
+  return <div>OverviewChart</div>;
+};
+
+export default OverviewChart;
